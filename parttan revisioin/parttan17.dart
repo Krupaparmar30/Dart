4e2f39a4209a0f162e39b0bc4d3685fc2dce02d6
krupa parmar <149374671+Krupaparmar30@users.noreply.github.com>
2024-11-26 12:29:14 +0530

    Add files via upload

---
+++ parttan revisioin/parttan17.dart
@@ -0,0 +1,36 @@
+
+
+// 1 2 3 4 5 
+// 2 3 4 5 6
+// 3 4 5 6 7 
+// 4 5 6 7 8
+// 5 6 7 8 9
+import 'dart:io';
+void main()
+{
+int k=1;
+int l,m;
+
+  for(int i=1;i<=5 ;i++)
+  {
+  
+    for (int j = 1;j <= 5;j++) {
+
+   k=i+j;
+   l=k*2;
+   m=l-3;
+
+  stdout.write('${m} ');
+        
+
+}
+        print("");
+         
+    
+
+  }
+
+
+ 
+
+}
